3c623f9f7fcf41e79c90f40e7460891317ac8e44
594652770@qq.com <594652770@qq.com>
2017-03-27 18:14:15 +0800

    首页

--- app/src/main/java/com/example/jjt/ui/home/adapter/HomeAdapter.java
+++ app/src/main/java/com/example/jjt/ui/home/adapter/HomeAdapter.java
@@ -3,13 +3,25 @@ package com.example.jjt.ui.home.adapter;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.example.jjt.R;
+import com.example.jjt.component.ImageLoader;
 import com.example.jjt.model.bean.HomeArticleModel;
 import com.example.jjt.model.bean.HomeMenuModel;
 import com.example.jjt.model.bean.HomeShopModel;
+import com.example.jjt.model.http.api.JJTApis;
+import com.example.jjt.widget.MyGridView;
+import com.example.jjt.widget.RatingBar;
 
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Administrator on 2017/3/25 0025.
@@ -22,11 +34,12 @@ public class HomeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private LayoutInflater inflater;
     private Context mContext;
 
+    private HomeMenuAdapter menuAdapter;
+
     public enum ITEM_TYPE {
-        ITEM_MENU,       //分类
-        ITEM_WENZHANG_TITLE,//文章title
+        ITEM_MENU,       //分类+文章title
         ITEM_WENZHANG,   //文章列表
-        ITEM_WENZHANG_LOAD, //文章加载
+//        ITEM_WENZHANG_LOAD, //文章加载
         ITEM_SHOP  //商店列表
     }
 
@@ -38,19 +51,138 @@ public class HomeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         inflater = LayoutInflater.from(mContext);
     }
 
+    @Override
+    public int getItemViewType(int position) {
+//        return super.getItemViewType(position);
+//        unreachable statement 永远都执行不到
+        if(position==0){
+            return ITEM_TYPE.ITEM_MENU.ordinal();
+        }else if(position<=mArticleList.size()){
+            return ITEM_TYPE.ITEM_WENZHANG.ordinal();
+        }
+//        else if(position==mArticleList.size()+1){
+//            return ITEM_TYPE.ITEM_WENZHANG_LOAD.ordinal();
+//        }
+        else{
+            return ITEM_TYPE.ITEM_SHOP.ordinal();
+        }
+    }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        if(viewType==ITEM_TYPE.ITEM_MENU.ordinal()){
+            menuAdapter=new HomeMenuAdapter(mContext,mMenuList);
+            return new MenuViewHolder(inflater.inflate(R.layout.item_home_top_one,parent,false));
+        }else if(viewType==ITEM_TYPE.ITEM_WENZHANG.ordinal()){
+            return new ArticleViewHolder(inflater.inflate(R.layout.item_home_top_two,parent,false));
+        }else {
+            return new ShopViewHolder(inflater.inflate(R.layout.item_home_top_three,parent,false));
+        }
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if(holder instanceof MenuViewHolder){
+            ((MenuViewHolder) holder).my_gridview.setAdapter(menuAdapter);
+        }else if(holder instanceof ArticleViewHolder){
+            ImageLoader.load(mContext, JJTApis.PicHost+mArticleList.get(position-1).getImage(),((ArticleViewHolder) holder).siv_zx_pic);
+            ((ArticleViewHolder) holder).tv_zx_title.setText(mArticleList.get(position-1).getTitle());
+            ((ArticleViewHolder) holder).tv_zx_content.setText(mArticleList.get(position-1).getShort_content());
+            ((ArticleViewHolder) holder).tv_zx_time.setText(mArticleList.get(position-1).getCreate_time());
+            ((ArticleViewHolder) holder).tv_zx_see.setText(mArticleList.get(position-1).getView()+"");
+
+//            if (position == mZxlist.size() - 1) {//显示加载
+//                holderView3.tv_zx_add_more.setVisibility(View.VISIBLE);
+//                if (zixun) {
+//                    holderView3.tv_zx_add_more.setText("暂无更多资讯，敬请期待");
+//                    holderView3.tv_zx_add_more.setEnabled(false);
+//                } else {
+//                    final HolderView3 finalHolderView = holderView3;
+//                    holderView3.tv_zx_add_more.setOnClickListener(new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View v) {
+//                            mZxlist2 = mZxlist;
+//                            wen_load = true;
+//                            wen_page++;
+//                            loadzxlist();
+//                            finalHolderView.tv_zx_add_more.setVisibility(View.GONE);
+//                        }
+//                    });
+//                }
+//            } else {
+//                holderView3.tv_zx_add_more.setVisibility(View.GONE);
+//            }
+        }else if(holder instanceof ShopViewHolder){
+            ImageLoader.load(mContext,JJTApis.PicHost+mShopList.get(position-1-mArticleList.size()).getHeadpic(),((ShopViewHolder)holder).siv_goods_pic);
+            ((ShopViewHolder)holder).tv_ct_name.setText(mShopList.get(position-1-mArticleList.size()).getShopname());
+            ((ShopViewHolder)holder).rtb_star.setStar(Float.parseFloat(mShopList.get(position-1-mArticleList.size()).getCmt_star()));
+            ((ShopViewHolder)holder).tv_ct_level.setText(mShopList.get(position-1-mArticleList.size()).getCmt_star()+"分");
+            ((ShopViewHolder)holder).tv_bili.setText(mShopList.get(position-1-mArticleList.size()).getDistance());
+
+            String tag = "";
+            for (int i = 0; i < mShopList.get(position-1-mArticleList.size()).getCates().size(); i++) {
+                tag += mShopList.get(position-1-mArticleList.size()).getCates().get(i) + " ";
+            }
+            ((ShopViewHolder)holder).tv_ct_tag.setText(tag);
+            ((ShopViewHolder)holder).tv_ct_length.setText("积分返还比例:"+Float.parseFloat(mShopList.get(position-1-mArticleList.size()).getCent_rate())*100+"%");
+        }
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 1+mArticleList.size()+mShopList.size();
+    }
+
+    public static class MenuViewHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.my_gridview)
+        MyGridView my_gridview;
+        @BindView(R.id.rl_newest_zx)
+        RelativeLayout rl_newest_zx;
+
+        public MenuViewHolder(View itemView){
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+        }
+    }
+
+    public static class ArticleViewHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.tv_zx_add_more)
+        TextView tv_zx_add_more;
+        @BindView(R.id.tv_zx_content)
+        TextView tv_zx_content;
+        @BindView(R.id.tv_zx_title)
+        TextView tv_zx_title;
+        @BindView(R.id.tv_zx_time)
+        TextView tv_zx_time;
+        @BindView(R.id.tv_zx_see)
+        TextView tv_zx_see;
+        @BindView(R.id.siv_zx_pic)
+        ImageView siv_zx_pic;
+        public ArticleViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+        }
+    }
+
+    public static class ShopViewHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.tv_ct_name)
+        TextView tv_ct_name;
+        @BindView(R.id.siv_goods_pic)
+        ImageView siv_goods_pic;
+        @BindView(R.id.rtb_star)
+        RatingBar rtb_star;
+        @BindView(R.id.tv_ct_level)
+        TextView tv_ct_level;
+        @BindView(R.id.tv_ct_length)
+        TextView tv_ct_length;
+        @BindView(R.id.tv_ct_tag)
+        TextView tv_ct_tag;
+        @BindView(R.id.tv_bili)
+        TextView tv_bili;
+        public ShopViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+        }
     }
 }
--- app/src/main/java/com/example/jjt/ui/home/fragment/HomeFragment.java
+++ app/src/main/java/com/example/jjt/ui/home/fragment/HomeFragment.java
@@ -1,8 +1,11 @@
 package com.example.jjt.ui.home.fragment;
 
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.example.jjt.Presenter.HomePresenter;
@@ -16,7 +19,9 @@ import com.example.jjt.model.bean.HomeMenuModel;
 import com.example.jjt.model.bean.HomeShopModel;
 import com.example.jjt.model.http.api.JJTApis;
 import com.example.jjt.ui.home.adapter.HomeAdapter;
+import com.example.jjt.utils.LogUtil;
 import com.example.jjt.utils.SnackbarUtil;
+import com.example.jjt.utils.SystemUtil;
 
 import java.util.List;
 
@@ -34,6 +39,10 @@ public class HomeFragment extends BaseFragment<HomePresenter> implements HomeCon
     RecyclerView rvContent;
     @BindView(R.id.swipe_refresh)
     SwipeRefreshLayout swipeRefresh;
+    @BindView(R.id.appbar)
+    AppBarLayout appbar;
+    @BindView(R.id.tabLayout)
+    TabLayout tabLayout;
     private List<HomeMenuModel.CatesBean> mMenuList;
     private List<HomeArticleModel> mArticleList;
     private List<HomeShopModel.CustomersBean> mShopList;
@@ -42,6 +51,7 @@ public class HomeFragment extends BaseFragment<HomePresenter> implements HomeCon
     private String pageSize="3";
     private int pageshop=1;
     private String show_type="",lng="",lat="";
+    private boolean isLoadingMoreShop=false;
     @Override
     protected void initInject() {
         getFragmentComponent().inject(this);
@@ -55,7 +65,48 @@ public class HomeFragment extends BaseFragment<HomePresenter> implements HomeCon
     @Override
     protected void initEventAndData() {
         mPresenter.getCarousel();//获取banner
+        rvContent.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                int lastVisibleItem = ((LinearLayoutManager) rvContent.getLayoutManager()).findLastVisibleItemPosition();
+                int totalItemCount = rvContent.getLayoutManager().getItemCount();
+                if (lastVisibleItem >= totalItemCount - 2 && dy > 0) {  //还剩2个Item时加载更多
+                    if(!isLoadingMoreShop){
+                        isLoadingMoreShop = true;
+                        pageshop++;
+                        mPresenter.getShop(pageshop+"",show_type,lng,lat);
+                        tabLayout.setVisibility(View.VISIBLE);
+                        tabLayout.getTabAt(1).select();
+                    }
+                }
+            }
+        });
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                pageshop=1;
+                mPresenter.getShop(pageshop+"",show_type,lng,lat);
+            }
+        });
 
+        appbar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (verticalOffset >= 0) {
+                    swipeRefresh.setEnabled(true);
+                } else {
+                    swipeRefresh.setEnabled(false);
+                    float rate = (float)(SystemUtil.dp2px(mContext, 256) + verticalOffset * 2) / SystemUtil.dp2px(mContext, 256);
+                    if (rate >= 0)
+                        ivScrollpic.setAlpha(rate);
+                }
+            }
+        });
+
+        tabLayout.addTab(tabLayout.newTab().setText("全部餐厅"));
+        tabLayout.addTab(tabLayout.newTab().setText("推荐餐厅"));
+        tabLayout.addTab(tabLayout.newTab().setText("附近餐厅"));
     }
 
     @Override
@@ -73,6 +124,7 @@ public class HomeFragment extends BaseFragment<HomePresenter> implements HomeCon
 
     @Override
     public void showArticle(List<HomeArticleModel> homeArticleModels) {
+
         mArticleList=homeArticleModels;
         mPresenter.getShop(pageshop+"",show_type,lng,lat);
 
@@ -85,10 +137,19 @@ public class HomeFragment extends BaseFragment<HomePresenter> implements HomeCon
 
     @Override
     public void showShop(HomeShopModel homeShopModel) {
-        mShopList=homeShopModel.getCustomers();
-        adapter=new HomeAdapter(mContext,mMenuList,mArticleList,mShopList);
-        rvContent.setLayoutManager(new LinearLayoutManager(mContext));
-        rvContent.setAdapter(adapter);
+        if(swipeRefresh.isRefreshing()){
+            swipeRefresh.setRefreshing(false);
+        }
+        if(isLoadingMoreShop){
+            mShopList.addAll(homeShopModel.getCustomers());
+            isLoadingMoreShop=false;
+            adapter.notifyDataSetChanged();
+        }else{
+            mShopList=homeShopModel.getCustomers();
+            adapter=new HomeAdapter(mContext,mMenuList,mArticleList,mShopList);
+            rvContent.setLayoutManager(new LinearLayoutManager(mContext));
+            rvContent.setAdapter(adapter);
+        }
     }
 
     @Override
@@ -106,5 +167,6 @@ public class HomeFragment extends BaseFragment<HomePresenter> implements HomeCon
 //            ivProgress.stop();
         }
         SnackbarUtil.showShort(rvContent,msg);
+        LogUtil.d(msg);
     }
 }
